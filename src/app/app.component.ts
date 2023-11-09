716ab0f2e0711075ecdc6533db533240cb2e2adb
Bram Borggreve <beeman@beeman.nl>
2023-11-09 16:52:03 +0000

    feat: add Tailwind

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,14 +7,18 @@ import { RouterLink, RouterOutlet } from '@angular/router'
   standalone: true,
   imports: [CommonModule, RouterOutlet, RouterLink],
   template: `
-    <h1>Welcome to {{ title }}!</h1>
-    <nav style="display: flex; gap: 4px;">
-      <a routerLink="/">Home</a>
-      <a routerLink="/about">About</a>
-      <a routerLink="/contact">Contact</a>
-      <a routerLink="/posts">Posts</a>
-    </nav>
-    <router-outlet></router-outlet>
+    <div class="space-y-4">
+      <h1 class="bg-red-800 p-4 text-red-200 text-2xl">Welcome to {{ title }}!</h1>
+      <nav class="px-4 space-x-2">
+        <a class="px-4 p-2 rounded-lg bg-black text-white" routerLink="/">Home</a>
+        <a class="px-4 p-2 rounded-lg bg-black text-white" routerLink="/about">About</a>
+        <a class="px-4 p-2 rounded-lg bg-black text-white" routerLink="/contact">Contact</a>
+        <a class="px-4 p-2 rounded-lg bg-black text-white" routerLink="/posts">Posts</a>
+      </nav>
+      <div class="p-4">
+        <router-outlet></router-outlet>
+      </div>
+    </div>
   `,
 })
 export class AppComponent {
